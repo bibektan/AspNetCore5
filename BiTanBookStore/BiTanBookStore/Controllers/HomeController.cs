0c8a240a52ae8102bb7b6e81b415bca8a9c042bb
Bibek Tan <bibektan@gmail.com>
2021-11-16 08:31:44 +0545

    Added MVC to application

---
+++ BiTanBookStore/BiTanBookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiTanBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string index()
+        {
+            return "home";
+        }
+    }
+}
